cda92810f5382647b56258556d2238f37c8ee79e
IlyaLyamin <94840814+IlyaLyamin@users.noreply.github.com>
2022-04-02 11:20:59 +0500

    создал доску объявлений, осталось только прикрепить фото из базы данных

---
+++ код/data/products.py
@@ -0,0 +1,18 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from sqlalchemy import orm
+from flask_login import UserMixin
+from sqlalchemy_serializer import SerializerMixin
+
+
+class Products(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'products'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    author = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
+    product = sqlalchemy.Column(sqlalchemy.String, default='без названия')
+    price = sqlalchemy.Column(sqlalchemy.Integer)
+    photo = sqlalchemy.Column(sqlalchemy.BLOB, default=None)
+    is_active = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+
+    user = orm.relation('User')
--- код/main.py
+++ код/main.py
@@ -1,8 +1,23 @@
 from flask import Flask, redirect, render_template, request, abort, jsonify
 from flask import make_response
+from data import db_session
+from data.products import Products
+from data.users import User
+from flask_login import LoginManager, login_user, login_required, current_user
+from base64 import b16decode, b16encode
+from io import BytesIO
 
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 @app.route('/authorization')
@@ -13,8 +28,15 @@ def authorization():
 @app.route('/')
 @app.route('/index')
 def home():
-    return render_template('index.html', title='Главная')
+    db_sess = db_session.create_session()
+    products = db_sess.query(Products).all()
+    return render_template('index.html', title='Главная', products=products)
+
+
+def main():
+    db_session.global_init('buynet.sqlite')
+    app.run()
 
 
 if __name__ == '__main__':
-    app.run()
+    main()
