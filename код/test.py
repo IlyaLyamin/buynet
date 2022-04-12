abd8e5ecef442e5b8dc385ed73be663d2d8fe850
IlyaLyamin <94840814+IlyaLyamin@users.noreply.github.com>
2022-04-12 11:40:36 +0500

    First release

--- код/test.py
+++
@@ -1,7 +0,0 @@
-from requests import get, post, delete
-
-print(get('http://localhost:5000/api/products').json())
-
-print(delete('http://localhost:5000/api/products/6').json())
-
-print(get('http://localhost:5000/api/products').json())
